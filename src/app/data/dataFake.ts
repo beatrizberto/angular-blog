0eb6767012d7c7e081a06ec16204e48f3edcee28
Beatriz <beatrizberto@gmail.com>
2023-05-26 18:28:29 -0300

    novas imagens adicionadas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,14 +2,14 @@ export const dataFake = [
   {
     id: '1',
     photoCover:
-      "'https://middermusic.com/wp-content/uploads/2023/03/best-female-djs.jpg'",
+      "../../../assets/imgs/bares.jpg",
     title: 'MTNÊ',
     description: '',
   },
   {
     id: '2',
     photoCover:
-      "'https://middermusic.com/wp-content/uploads/2023/03/best-female-djs.jpg'",
+      "../../../assets/imgs/bares.jpg",
     title: 'Festa Roxa',
     description:
       'A Festa Roxa é projeto idealizado por Ellen Quintanilla, Ale Hofty e Ana Paula Dsc (RJ). Com edições no Rio de Janeiro e em São Paulo, a festa é espaço de liberdade, diversão e inclusão para pessoas de todas a orientações sexuais e identidades de gênero. Com Ellen e Ale como DJs residentes, conta sempre com a presença de DJs convidados a tocar os mais diversos estilos, do house ao funk.  ',
@@ -17,7 +17,7 @@ export const dataFake = [
   {
     id: '3',
     photoCover:
-      "'https://middermusic.com/wp-content/uploads/2023/03/best-female-djs.jpg'",
+      "../../../assets/imgs/bares.jpg",
     title: 'MTNÊ',
     description: 'Idealizada pela DJ MT Cranmarry (SP), a festa busca resgatar as Matinês dos anos 1990 e começo da década de 2000. Os DJs residentes tocam sets curtos e hiperdançantes com o melhor da dance music dos últimos 30 anos. A festa começou online durante a pandemia, transmitida pela Rádio Cafuné, e já teve sua primeira edição presencial em São Paulo. ',
   },
